ed1ee9bb4f40d09d2e7c570086eb72062d0a68f3
Jeremy Cloarec <159018898+JeremyCloarec@users.noreply.github.com>
2025-09-16 18:29:05 +0200

    Issue/aes hybrid encoding (#28)
    use hybrid aes encoding

--- src/api/opencti/connector/mod.rs
+++ src/api/opencti/connector/mod.rs
@@ -12,12 +12,20 @@ pub mod post_health;
 use cynic;
 use base64::{Engine as _, engine::general_purpose};
 use crate::api::opencti::opencti as schema;
+use aes_gcm::{
+    aead::{Aead, KeyInit},
+    Aes256Gcm, Nonce
+};
 
 #[derive(cynic::QueryFragment, Debug, Clone, Serialize)]
 pub struct ConnectorContractConfiguration {
     pub key: String,
     pub value: Option<String>,
-    pub encrypted: Option<bool>
+    pub encrypted: Option<bool>,
+    #[cynic(rename = "encryptionKey")]
+    pub encryption_key: Option<String>,
+    #[cynic(rename = "encryptionIv")]
+    pub encryption_iv: Option<String>
 }
 
 #[derive(cynic::QueryFragment, Debug, Clone)]
@@ -38,25 +46,26 @@ pub struct ManagedConnector {
 
 impl ManagedConnector {
 
-    pub fn parse_encrypted_field(&self,  private_key: &RsaPrivateKey, encrypted_value: String) -> String {
-        let encrypted_bytes_result = general_purpose::STANDARD.decode(encrypted_value);
-        match encrypted_bytes_result {
-            Ok(encrypted_bytes) => {
-                let decoded_data_result = private_key.decrypt(Pkcs1v15Encrypt, &encrypted_bytes);
-                match decoded_data_result {
-                    Ok(decoded_data) => {
-                        let dec_data_as_str = str::from_utf8(&decoded_data).unwrap().to_string();
-                        dec_data_as_str
-                    }
-                    Err(..) => {
-                        warn!("Incorrect encrypted data decrypt");
-                        String::from("")
-                    }
-                }
-            }
-            Err(..) => {
-                warn!("Incorrect value bas64 decode");
-                String::from("")
+    pub fn parse_aes_encrypted_value(&self,  private_key: &RsaPrivateKey, encrypted_value: String, encrypted_aes_key: String, encrypted_aes_iv: String) -> Result<String, Box<dyn std::error::Error>> {
+        let aes_key_encrypted_bytes = general_purpose::STANDARD.decode(encrypted_aes_key)?;
+        let aes_key_decrypted_bytes = private_key.decrypt(Pkcs1v15Encrypt, &aes_key_encrypted_bytes)?;
+
+        let aes_iv_encrypted_bytes = general_purpose::STANDARD.decode(encrypted_aes_iv)?;
+        let aes_iv_decrypted_bytes = private_key.decrypt(Pkcs1v15Encrypt, &aes_iv_encrypted_bytes)?;
+
+        let encrypted_value_bytes = general_purpose::STANDARD.decode(encrypted_value)?;
+
+        let cipher = Aes256Gcm::new_from_slice(&aes_key_decrypted_bytes)?;
+        let nonce = Nonce::from_slice(&aes_iv_decrypted_bytes);
+        let plaintext_result = cipher.decrypt(&nonce, encrypted_value_bytes.as_ref());
+        match plaintext_result {
+            Ok(plaintext) => {
+                let decoded_value = str::from_utf8(&plaintext).unwrap().to_string();
+                Ok(decoded_value)
+            },
+            Err(e) => {
+                warn!(error = e.to_string(), "Fail to decode value");
+                Ok(String::from(""))
             }
         }
     }
@@ -70,12 +79,24 @@ impl ManagedConnector {
             .map(|c| {
                 let is_sensitive = c.encrypted.unwrap_or_default();
                 if is_sensitive {
-                    let value = c.value.unwrap_or_default();
-                    let decoded_value = self.parse_encrypted_field(private_key, value);
-                    ApiContractConfig {
-                        key: c.key,
-                        value: decoded_value,
-                        is_sensitive: true,
+                    let encrypted_value = c.value.unwrap_or_default();
+                    let encrypted_key = c.encryption_key.unwrap_or_default();
+                    let encrypted_iv = c.encryption_iv.unwrap_or_default();
+                    let decoded_value_result = self.parse_aes_encrypted_value(private_key, encrypted_value, encrypted_key, encrypted_iv);
+                    match decoded_value_result {
+                        Ok(decoded_value) => ApiContractConfig {
+                            key: c.key,
+                            value: decoded_value,
+                            is_sensitive: true,
+                        },
+                        Err(e) => {
+                            warn!(error = e.to_string(), "Fail to decode value");
+                            ApiContractConfig {
+                                key: c.key,
+                                value: String::from(""),
+                                is_sensitive: true,
+                            }
+                        }
                     }
                 } else {
                     ApiContractConfig {
